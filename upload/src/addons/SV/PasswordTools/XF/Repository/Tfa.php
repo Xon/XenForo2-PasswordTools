cceb4c8fd2c034a39031e79ce7ac7537d93049ee
Xon <tazipper1@hotmail.com>
2021-10-21 22:31:15 +0800

    Fix remember-device+2fa support

--- upload/src/addons/SV/PasswordTools/XF/Repository/Tfa.php
+++ upload/src/addons/SV/PasswordTools/XF/Repository/Tfa.php
@@ -99,6 +99,13 @@ class Tfa extends XFCP_Tfa
             return true;
         }
 
+        /** @var \XF\Repository\UserTfaTrusted $tfaTrustRepo */
+        $tfaTrustRepo = $this->repository('XF:UserTfaTrusted');
+        if ($trustKey && $tfaTrustRepo->getTfaTrustRecord($user->user_id, $trustKey))
+        {
+            return false;
+        }
+
         if ($this->isSvForcedEmail2fa($user))
         {
             // user has a compromised password, force 2fa. login/two-step forces email 2fa enabled
