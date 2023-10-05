b8e00f5d08a048f703a502ea94fa963aa9cb766c
Paul Buonopane <paul@namepros.com>
2023-10-05 11:28:35 -0400

    Convert tabs to spaces

--- upload/src/addons/SV/PasswordTools/_files/js/sv/passwordtools/password_box.js
+++ upload/src/addons/SV/PasswordTools/_files/js/sv/passwordtools/password_box.js
@@ -33,85 +33,85 @@
             }
 
             this.svPasswordToolsRejectFragments = rejectFragments.length ? rejectFragments : null;
-			this.svPasswordToolsForceReject = data.forceReject;
+            this.svPasswordToolsForceReject = data.forceReject;
         },
 
         input: function()
         {
-			if (!this.svPasswordToolsRejectFragments)
-			{
-				this.superInput();
-				return;
-			}
+            if (!this.svPasswordToolsRejectFragments)
+            {
+                this.superInput();
+                return;
+            }
 
-			var password = this.$password.val(),
-				result = zxcvbn(password, this.svPasswordToolsRejectFragments || []),
-				score = result.score,
-				value,
-				message = result.feedback.warning || '',
-				messageExtra = null;
+            var password = this.$password.val(),
+                result = zxcvbn(password, this.svPasswordToolsRejectFragments || []),
+                score = result.score,
+                value,
+                message = result.feedback.warning || '',
+                messageExtra = null;
 
-			// note: the messages in this file are translated elsewhere
+            // note: the messages in this file are translated elsewhere
 
 
-			if (password === '')
-			{
-				message = 'Entering a password is required';
-				value = 0;
-			}
-			else
-			{
-				if (this.svPasswordToolsForceReject)
-				{
-					var matchedWords = [];
+            if (password === '')
+            {
+                message = 'Entering a password is required';
+                value = 0;
+            }
+            else
+            {
+                if (this.svPasswordToolsForceReject)
+                {
+                    var matchedWords = [];
 
-					for (var i = 0; i < result.sequence.length; i++)
-					{
-						var item = result.sequence[i];
-						if (item.pattern === 'dictionary' && item.dictionary_name === 'user_inputs')
-						{
-							matchedWords.push(item.token);
+                    for (var i = 0; i < result.sequence.length; i++)
+                    {
+                        var item = result.sequence[i];
+                        if (item.pattern === 'dictionary' && item.dictionary_name === 'user_inputs')
+                        {
+                            matchedWords.push(item.token);
 
-							if (item.token.toLowerCase() !== item.matched_word.toLowerCase())
-							{
-								matchedWords.push(item.matched_word);
-							}
-						}
-					}
+                            if (item.token.toLowerCase() !== item.matched_word.toLowerCase())
+                            {
+                                matchedWords.push(item.matched_word);
+                            }
+                        }
+                    }
 
-					if (matchedWords.length)
-					{
-						message = "Your password can't contain any variation of the following phrase(s):";
-						score = 0;
-						messageExtra = matchedWords.join(", ");
-					}
-				}
+                    if (matchedWords.length)
+                    {
+                        message = "Your password can't contain any variation of the following phrase(s):";
+                        score = 0;
+                        messageExtra = matchedWords.join(", ");
+                    }
+                }
 
-				value = (score + 1) * 20;
+                value = (score + 1) * 20;
 
-				if (score >= 4)
-				{
-					message = 'This is a very strong password';
-				}
-				else if (score >= 3)
-				{
-					message = 'This is a reasonably strong password';
-				}
-				else if (!message)
-				{
-					message = 'The chosen password could be stronger';
-				}
-			}
+                if (score >= 4)
+                {
+                    message = 'This is a very strong password';
+                }
+                else if (score >= 3)
+                {
+                    message = 'This is a reasonably strong password';
+                }
+                else if (!message)
+                {
+                    message = 'The chosen password could be stronger';
+                }
+            }
 
-			message = this.language[message] || message;
+            message = this.language[message] || message;
 
-			if (messageExtra !== null)
-			{
-				message += ' ' + messageExtra;
-			}
+            if (messageExtra !== null)
+            {
+                message += ' ' + messageExtra;
+            }
 
-			this.$meter.val(value);
-			this.$meterText.text(message);
+            this.$meter.val(value);
+            this.$meterText.text(message);
         },
     });
 }();
