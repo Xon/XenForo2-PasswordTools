298ee479b53195758f58fe8986dd0c30a376a40e
Xon <635541+Xon@users.noreply.github.com>
2023-12-19 06:46:20 +0800

    Add "Force password reset on compromised password" option

--- upload/src/addons/SV/PasswordTools/XF/Service/User/Login.php
+++ upload/src/addons/SV/PasswordTools/XF/Service/User/Login.php
@@ -28,20 +28,16 @@ class Login extends XFCP_Login
 
             $options = \XF::options();
             $alertOnCompromisedPassword = (bool)($options->svAlertOnCompromisedPasswordOnLogin ?? true);
+            $forcePasswordResetOnCompromisedPassword = (bool)($options->svPwnedPasswordForcePasswordReset ?? false);
             $pwnedPasswordGroupId = (int)($options->svPwnedPasswordGroup ?? 0);
             $forceEmail2fa = $auth->svIsForceEmail2Fa();
-            if (!$pwnedPasswordGroupId && !$alertOnCompromisedPassword && !$forceEmail2fa)
-            {
-                return $user;
-            }
-
             // If sv_pwned_password_check is non-empty, this implies the last compromised password check failed
             // The value will be reset to null on the next password change, allowing 2fa to be forced if configured without additional compromised password checks
             $lastPwnedPasswordCheck = $auth->sv_pwned_password_check ?? 0;
             $recurring = (int)($options->svPwnedPasswordAlertRecurring ?? 24) * 60*60;
             $sendCompromisedPasswordAlert = $alertOnCompromisedPassword && ($lastPwnedPasswordCheck + $recurring < \XF::$time);
 
-            if ($sendCompromisedPasswordAlert || $forceEmail2fa || $pwnedPasswordGroupId !== 0)
+            if ($sendCompromisedPasswordAlert || $forcePasswordResetOnCompromisedPassword || $forceEmail2fa || $pwnedPasswordGroupId !== 0)
             {
                 // the pwned password check needs to run after the password validation, but before the 2fa check
                 // otherwise the 'Force email two-factor authentication on compromised password' option will not reliably trigger
@@ -59,6 +55,12 @@ class Login extends XFCP_Login
                             /** @var UserGroupChangeService $userGroupChangeService */
                             $userGroupChangeService = \XF::app()->service(UserGroupChangeService::class);
                             $userGroupChangeService->addUserGroupChange($user->user_id, 'svCompromisedPassword', $pwnedPasswordGroupId);
+                        }
+
+                        if ($forcePasswordResetOnCompromisedPassword && $user->security_lock === '')
+                        {
+                            $user->security_lock = 'reset';
+                            $user->save();
                         }
 
                         $db->commit();
