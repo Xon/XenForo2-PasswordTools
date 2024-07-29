158ea48f32a0f6dc7dcd8556d0f63c7030065e79
Xon <635541+Xon@users.noreply.github.com>
2024-07-30 00:15:51 +0800

    Update XF bug link to be more easily grepable

--- upload/src/addons/SV/PasswordTools/XF/Repository/User.php
+++ upload/src/addons/SV/PasswordTools/XF/Repository/User.php
@@ -12,8 +12,7 @@ class User extends XFCP_User
     {
         $user = parent::setupBaseUser($user);
 
-        // XF bug; the created entities don't link back to the User entity
-        // https://xenforo.com/community/threads/userrepo-setupbaseuser-doesnt-setup-dependant-entities-as-expected.223540/
+        // XF bug; the created entities don't link back to the User entity https://xenforo.com/community/threads/userrepo-setupbaseuser-doesnt-setup-dependant-entities-as-expected.223540/
         $user->Option->hydrateRelation('User', $user);
         $user->Profile->hydrateRelation('User', $user);
         $user->Privacy->hydrateRelation('User', $user);
